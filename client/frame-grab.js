0417ddfbc5eb12ad4f099af5d6a10edc0eddf677
Ray Nicholus <github.com@garstasio.com>
2014-02-19 22:57:21 -0600

    feat($grab-one): shorten vars #1
    TODO: Unit tests

--- client/frame-grab.js
+++ client/frame-grab.js
@@ -23,35 +23,34 @@
         },
 
         _timecode_to_secs: function(timecode, frame_rate) {
-            var timecode_segments = timecode.split(":").reverse(),
-                seconds_per_frame = 1 / frame_rate,
-                seconds = 0;
+            var segments = timecode.split(":").reverse(),
+                secs_per_frame = 1 / frame_rate,
+                secs = 0;
 
             for (var i = 3; i >= 0; i--) {
                 switch (i) {
                     case 0:
-                        seconds += parseInt(timecode_segments[i]) *
-                            seconds_per_frame;
+                        secs += parseInt(segments[i]) * secs_per_frame;
                         break;
                     case 1:
-                        if (timecode_segments.length === 2) {
-                            seconds += parseInt(timecode_segments[i]);
+                        if (segments.length === 2) {
+                            secs += parseInt(segments[i]);
                         }
                         break;
                     case 2:
-                        if (timecode_segments.length === 3) {
-                            seconds += parseInt(timecode_segments[i]) * 60;
+                        if (segments.length === 3) {
+                            secs += parseInt(segments[i]) * 60;
                         }
                         break;
                     case 3:
-                        if (timecode_segments.length === 4) {
-                            seconds += parseInt(timecode_segments[i]) * 60 * 60;
+                        if (segments.length === 4) {
+                            secs += parseInt(segments[i]) * 60 * 60;
                         }
                         break;
                 }
             }
 
-            return seconds;
+            return secs;
         }
     };
 
