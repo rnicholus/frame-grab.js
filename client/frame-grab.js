6782782b44698421fb4212586bb50befb2c16ce7
Ray Nicholus <github.com@garstasio.com>
2014-04-14 23:50:09 -0500

    chore(frame-grab.js) fix context & naming inconsistencies  #2

--- client/frame-grab.js
+++ client/frame-grab.js
@@ -117,28 +117,28 @@
 
                 clone_ready.then(function(cloned_video) {
                     var frame_period = cloned_video.duration / images,
-                        rendered_frames = [];
+                        rendered_frames = [],
 
-                    function drawNextFrame(time_to_render) {
-                        var container = document.createElement(normalized_type);
+                        draw_next_frame = function(time_to_render) {
+                            var container = document.createElement(normalized_type);
 
-                        this.grab(container, time_to_render, size).then(
-                            function grabbed(rendered_container) {
-                                rendered_frames.push(rendered_container);
+                            this.grab(container, time_to_render, size).then(
+                                function grabbed(rendered_container) {
+                                    rendered_frames.push(rendered_container);
 
-                                if (rendered_frames.length < images) {
-                                    drawNextFrame.call(this, time_to_render + frame_period);
-                                }
-                                else {
-                                    deferred.resolve(rendered_frames);
-                                }
-                            }.bind(this),
+                                    if (rendered_frames.length < images) {
+                                        draw_next_frame(time_to_render + frame_period);
+                                    }
+                                    else {
+                                        deferred.resolve(rendered_frames);
+                                    }
+                                }.bind(this),
 
-                            deferred.reject
-                        );
-                    }
+                                deferred.reject
+                            );
+                        }.bind(this);
 
-                    drawNextFrame.call(this, 0);
+                    draw_next_frame(0);
                 }.bind(this));
 
                 return deferred.promise;
